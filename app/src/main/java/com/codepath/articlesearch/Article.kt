6174cdeae9ae4a5943039475aefbc1e5ed611d86
Bmogul <98663272+Bmogul@users.noreply.github.com>
2023-10-05 12:28:25 -0400

    Basic data class completed

--- app/src/main/java/com/codepath/articlesearch/Article.kt
+++ app/src/main/java/com/codepath/articlesearch/Article.kt
@@ -1,1 +1,34 @@
 package com.codepath.articlesearch
+
+import android.support.annotation.Keep
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Keep
+@Serializable
+data class SearchNewsResponse(
+    @SerialName("response")
+    val response: BaseResponse?
+)
+
+@Keep
+@Serializable
+data class BaseResponse(
+    @SerialName("docs")
+    val docs: List<Article>?
+)
+
+@Keep
+@Serializable
+data class Article(
+    @SerialName("abstract")
+    val abstract: String?,
+    @SerialName("byline")
+    val byline: Byline?,
+    @SerialName("headline")
+    val headline: Headline?,
+    @SerialName("multimedia")
+    val multimedia: List<Multimedia>?,
+): java.io.Serializable {
+    val mediaImageUrl = "https://www.nytimes.com/${multimedia?.firstOrNull { it.url != null }?.url ?: ""}"
+}
